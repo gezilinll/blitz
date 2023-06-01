a93d9734748d394668da0aecb0c35bbc7a50d905
linbinghe <linbinghe@gmail.com>
2023-06-01 11:31:53 +0800

    feat: use vue-quill

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,9 @@
-import Quill from 'quill';
-import QuillCursors from 'quill-cursors';
-import 'quill/dist/quill.snow.css'; // 使用了 snow 主题色
-import * as Y from 'yjs';
-import { QuillBinding } from 'y-quill';
-import { WebsocketProvider } from 'y-websocket';
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
-// 使用 cursors 插件
-Quill.register('modules/cursors', QuillCursors);
-
-const quill = new Quill(document.querySelector('#app'), {
-    modules: {
-        cursors: true,
-        toolbar: [
-            [{ header: [1, 2, false] }],
-            ['bold', 'italic', 'underline'],
-            ['image', 'code-block'],
-        ],
-        history: {
-            userOnly: true,
-        },
-    },
-    placeholder: '前端西瓜哥...',
-    theme: 'snow',
-});
-
-const ydoc = new Y.Doc();
-const ytext = ydoc.getText('quill');
-const provider = new WebsocketProvider('wss://demos.yjs.dev', 'quill-demo-room', ydoc);
-const binding = new QuillBinding(ytext, quill, provider.awareness);
+const app = createApp(App);
+app.component('QuillEditor', QuillEditor)
+app.mount('#app')
