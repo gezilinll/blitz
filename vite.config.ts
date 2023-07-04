5f05193bf7c5a5bc30316397427b93782a31943f
linbinghe <linbinghe@gmail.com>
2023-07-04 22:19:24 +0800

    feat: compress dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { visualizer } from 'rollup-plugin-visualizer'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/',
-    plugins: [vue()],
+    plugins: [vue(), viteCompression(), visualizer()],
 });
