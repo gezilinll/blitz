9f57b94bdbcfa2c55fadf3c8568008a54fb80ed3
linbinghe <linbinghe@gmail.com>
2024-02-14 15:19:13 +0800

    feat: init app

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,23 @@
 <template>
-    <div>
-        <span> Hello World </span>
+    <div class="app-container">
+        <canvas ref="canvas" class="editor-fullscreen"></canvas>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { BlitzRenderer, GraphicsRendererPlugin } from '@blitz/canvas';
+import { Editor } from '@blitz/core';
+import { onMounted, ref } from 'vue';
+
+const canvas = ref(null);
+
+onMounted(() => {
+    const editor = new Editor();
+    const renderer = new BlitzRenderer(canvas.value!);
+    const graphicsRendererPlugin = new GraphicsRendererPlugin(renderer);
+    editor.registerPlugin(graphicsRendererPlugin);
+});
+</script>
 
 <style lang="less">
 body {
@@ -19,5 +32,11 @@ body {
     height: 100vh;
     margin: 0px;
     overflow: hidden;
+}
+
+.editor-fullscreen {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
 }
 </style>
